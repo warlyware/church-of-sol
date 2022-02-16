41d756ea02618522a21f6777d4a937cc91c8c346
Dan Ward <danward@gmail.com>
2022-02-15 21:10:44 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="h-full min-h-screen flex justify-center items-center">
+    <h1 class="text-9xl text-yellow-500 leading-tight">
+      praise<br>
+      the<br>
+      sol
+    </h1>
+  </div>
+</template>
+
+<style>
+#app {
+  @apply min-h-screen h-full bg-blue-800;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+h1 {
+  font-family: 'Cinzel Decorative', cursive;
+}
+</style>
